5e078789e20d51ce3a6ef2e518a19dd51e4e8672
smnatale <66391194+smnatale@users.noreply.github.com>
2026-09-14 13:20:14 +0100

    update cask syntax

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,20 +1,17 @@
 cask "neovim-nightly" do
+  arch arm: "arm64", intel: "x86_64"
+
   version :latest
   sha256 :no_check
 
-  arch arm: "arm64", intel: "x86_64"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
-      verified: "github.com/neovim"
-
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz"
   name "Neovim"
   desc "Vim-fork focused on extensibility and usability"
-  homepage "https://neovim.io"
-
-  conflicts_with formula: "neovim"
+  homepage "https://neovim.io/"
 
   binary "nvim-macos-#{arch}/bin/nvim"
 
-  postflight do
-    system_command "xattr", args: ["-cr", "#{staged_path}"]
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-cr", "{{staged_path}}"], must_succeed: false
   end
 end
